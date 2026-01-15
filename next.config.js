3719f894622077119395c2ee04608e9dbbe52766
XD ATIK VAU <145763843+XD-MADS-12@users.noreply.github.com>
2026-01-15 21:48:39 +0600

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/ next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    domains: ['placehold.co'],
+  },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    ADMIN_TOKEN: process.env.ADMIN_TOKEN,
+  },
+}
+
+module.exports = nextConfig
